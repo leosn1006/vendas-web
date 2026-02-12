68c63fc5195f7c167bb8f1506bd8146b81819793
Leo <leosn1006@gmail.com>
2026-02-12 18:39:49 -0300

    inclui webhook

---
+++ app/constante.py
@@ -0,0 +1,2 @@
+# url do Whatsapp Business API
+WHATSAPP_API_URL='https://graph.facebook.com/v24.0/'
---
+++ app/enviar_mensagem_whatsApp.py
@@ -0,0 +1,44 @@
+import requests
+import json
+import os
+from app.constante import WHATSAPP_API_URL
+
+def enviar_mensagem_whatsapp(msg_original_json, mensagem_resposta):
+    """
+    Envia uma mensagem de texto para o WhatsApp usando a API.
+    msg_original_json: O JSON original recebido do webhook do WhatsApp.
+    mensagem_resposta: O texto da mensagem que queremos enviar como resposta.
+    """
+
+    url = f"{WHATSAPP_API_URL}927793497092010/messages"
+    token = os.getenv('WHATSAPP_ACCESS_TOKEN', '')
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json; charset=utf-8",
+        "Accept": "application/json"
+    }
+
+    # Extrai o número do remetente e o ID da conversa do JSON original
+    try:
+        numero_remetente = msg_original_json['entry'][0]['changes'][0]['value']['messages'][0]['from']
+        id_conversa = msg_original_json['entry'][0]['changes'][0]['value']['messages'][0]['id']
+
+        dados = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": numero_remetente,
+            "type": "text",
+            "text": {
+                "preview_url": "true",
+                "body": mensagem_resposta
+                }
+            }
+        print(f"[MENSAGEM] Enviando resposta para {numero_remetente} (ID da conversa: {id_conversa})")
+
+        # executar uma chamada a API POST para enviar a mensagem
+        response = requests.post(url, headers=headers, json=dados)
+        response.raise_for_status()
+        print("[MENSAGEM] Mensagem enviada com sucesso!")
+
+    except Exception as e:
+        print(f"[MENSAGEM] Erro ao enviar mensagem: {e}")
