4a4580538a00916169e2055faefa1eaa8aa3dd6d
Leo <leosn1006@gmail.com>
2026-03-13 15:43:31 -0300

    ajustes dados do pedido com dados do cliente com pagador

--- migrations/012_campos_pagamento_bb_pay.sql
+++ migrations/012_campos_pagamento_bb_pay.sql
@@ -2,7 +2,7 @@
 -- CPF/CNPJ formatado (CNPJ pode ser alfanumérico a partir de jun/2026)
 
 ALTER TABLE pedidos
-  ADD COLUMN cpf_cnpj_pagador        VARCHAR(20)   NULL COMMENT 'CPF ou CNPJ formatado do pagador (BB Pay)'  AFTER nome_pagador,
-  ADD COLUMN valor_liquido_pagamento DECIMAL(10,2) NULL COMMENT 'valorLiquidoRecebedor BB Pay'               AFTER valor_pago,
-  ADD COLUMN data_repasse            DATE          NULL COMMENT 'dataRepassePagamento BB Pay'                 AFTER data_pagamento,
-  ADD COLUMN e2e_id                  VARCHAR(100)  NULL COMMENT 'e2eId PIX (BB Pay)'                         AFTER data_repasse;
+  ADD COLUMN cpf_cnpj_pagador VARCHAR(20)  NULL COMMENT 'CPF ou CNPJ formatado do pagador (BB Pay)' AFTER nome_pagador,
+  ADD COLUMN data_repasse     DATE         NULL COMMENT 'dataRepassePagamento BB Pay'                AFTER data_pagamento,
+  ADD COLUMN e2e_id           VARCHAR(100) NULL COMMENT 'e2eId PIX (BB Pay)'                        AFTER data_repasse;
+-- Nota: valor_liquido_pagamento já existia no banco (não recriada)
